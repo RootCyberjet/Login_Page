212f811b990b057205002a811eacdaade9eaaa19
SHIBA BISWAS <56774836+RootCyberjet@users.noreply.github.com>
2020-01-06 15:51:53 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html>
-  <head><title>Sign In Page</title></head>
-  <body style="background-color : green;text-align:center">
+  <head>
+	  <title>Sign In Page</title>
+	  
+	  <style>
+		body {background-color:green;text-align:center;}
+	        div {background-color:yellow;}
+	  </style>
+  </head>
+  <body>
   
 	  <h1><span style="background-color:yellow;color:red"><b><u>TESTING SITE</u></b></span></h1>
 	<h3><span style="background-color:yellow">N.B: Don't put your sensitive credentials in this website.</span></h3>
 	
 	
-	 <div style="background-color:yellow">
+	 <div>
 		<span style="color:blue;background-color:yellow"><b><p><u>NEW USER SIGN IN PAGE</u></p></b></span>
      	 <form action="process1.php" method="post">
 	   <span style="color:red"><b>Username :</b></span>  <input type="text" name="username" value="" placeholder="username" style="background-color:yellow" required><br><br>
