4f598aaee2129c273b2f920003f3a10c7154ce8f
SHIBA BISWAS <56774836+RootCyberjet@users.noreply.github.com>
2020-01-06 15:45:25 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -7,14 +7,16 @@
 	<h3><span style="background-color:yellow">N.B: Don't put your sensitive credentials in this website.</span></h3>
 	
 	
-	<div style="background-color:yellow"><span style="color:blue;background-color:yellow"><b><p><u>NEW USER SIGN IN PAGE</u></p></b></span>
-     <form action="process1.php" method="post">
+	 <div style="background-color:yellow">
+		<span style="color:blue;background-color:yellow"><b><p><u>NEW USER SIGN IN PAGE</u></p></b></span>
+     	 <form action="process1.php" method="post">
 	   <span style="color:red"><b>Username :</b></span>  <input type="text" name="username" value="" placeholder="username" style="background-color:yellow" required><br><br>
 	   <span style="color:red"><b>Password :</b></span>  <input type="password" name="password1" value="" placeholder="New Password" style="background-color:yellow" required><br><br>
-       <span style="color:red"><b>Verify New Password :</b></span>  <input type="password" name="password2" value="" placeholder="Verify New Password" style="background-color:yellow" required><br><br>
-       <input type="submit" value="Sign in" style="background-color:red">
+       	   <span style="color:red"><b>Verify New Password :</b></span>  <input type="password" name="password2" value="" placeholder="Verify New Password" style="background-color:yellow" required><br><br>
+           <input type="submit" value="Sign in" style="background-color:red">
 	 </form>
-	 <span style="color:blue;background-color:yellow"><p><b>MEMBER !!!!! <a href="http://localhost:8080/login.php">Log in</a></b></p></span></div>
+	   <span style="color:blue;background-color:yellow"><p><b>MEMBER !!!!! <a href="http://localhost:8080/login.php">Log in</a></b></p></span>
+	 </div>
 	<footer style="background-color:pink;color:red">
 		<h5>Posted by : RootCyberjet</h5>
 	</footer>
