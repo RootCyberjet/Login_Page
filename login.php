a2f347b23087307cade9464f644b9a84fcfc66b3
SHIBA BISWAS <56774836+RootCyberjet@users.noreply.github.com>
2020-01-06 16:01:42 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,19 +3,19 @@
   <head><title>Login Page</title></head>
   <body style="background-color :green;text-align:center">
   
-	<h1 style="color:red"><span style="background-color:yellow"><b><u>TESTING SITE</b></u></span></h1>
+	<h1 style="color:red"><span style="background-color:yellow"><b><u>TESTING SITE</u></b></span></h1>
 	<h3><span style="background-color:yellow">N.B: Don't put your sensitive credentials in this website.</span></h3>
 	
 	
-	<div style="background-color:yellow"><b><p><span style="color:blue;background-color:yellow"><u>EXISTING USER'S LOGIN PAGE</u></span></P></b>
+	<div style="background-color:yellow"><b><p><span style="color:blue;background-color:yellow"><u>EXISTING USER'S LOGIN PAGE</u></span></p></b>
      <form action="process.php" method="post">
 	   <span style="color:red"><b>Username :</b>  <input type="text" name="username" value="" placeholder="username" style="background-color:yellow" required><br><br>
 	   <span style="color:red"><b>Password :</b>  <input type="password" name="password" value="" placeholder="password" style="background-color:yellow" required><br><br>
        <input type="submit" value="Login" style="background-color:red">
 	 </form>
-	<p><b><span style="color:blue;background-color:yellow">NOT A MEMBER !!!!! <a href="http://localhost:8080/signin.php">Sign in</a></span></b></P></div>
+	<p><b><span style="color:blue;background-color:yellow">NOT A MEMBER !!!!! <a href="http://localhost:8080/signin.php">Sign in</a></span></b></p></div>
 	<footer style="background-color:pink;color:red">
-		<h5><span >Posted by : RootCyberjet</h5>
+		<h5><span >Posted by : RootCyberjet</span></h5>
 	</footer>
   </body>
 </html>
